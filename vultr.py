4aaebb0fa7cdf1cdb94c5d4503790cfd201b2fd1
forget <53308945+forget@users.noreply.github.com>
2023-05-28 03:39:04 +0100

    Add files via upload

--- vultr.py
+++ vultr.py
@@ -1,26 +1,43 @@
+# VULTR API: https://www.vultr.com/api/
+# Creator: https://github.com/forget/
+
 from requests import Session
-from manager import Manager
-from time import sleep
 from threading import Thread
 
-class VULTR:
+from library.manager import Manager
+from library.logger import Logger
+from library.assistant import Assistant
+
+from logging import INFO, basicConfig
+from time import sleep
+
+basicConfig(
+    level=INFO, 
+    format='[%(levelname)s] [%(asctime)s] %(message)s', 
+    datefmt='%b %d | %H:%M', 
+    filename='deployment.log',
+)
+
+class VULTR(object):
     def __init__(self):
-        self.session = Session()
-        self.vultr_api_key = "" # your api key
-        self.servers = 1 # servers you need
-        self.label = "me" # the name of your instance
+        self.session: Session = Session()
+        self.manager: Manager = Manager()
+        self.logger: Logger = Logger()
         
-        self.active_servers = []
-                       
-        self.init()
-       
-    def create_instance(self, index):
-        try:
+        self.settings: object = self.manager.load_settings()
+        self.vultr_api_key: str = self.settings['vultr_api_key']
+        self.operator: str = self.settings['current_operator']
+        self.instances: int = self.settings['number_of_instances']
+        self.name: str = self.settings['server_name']
+        
+        self.running: bool = True
+                              
+    def create_instance(self, index: int) -> bool:
             data = {
-                'region': 'sjc',
-                'plan': 'vhf-1c-1gb',
-                'os_id': 387, # Ubuntu
-                'label': f'{self.label} [#{index}]'
+                'region': 'sea', # Seattle, US 
+                'plan': 'vhf-1c-1gb', # High frequency, 1 core 1 gb ram
+                'os_id': 387, # Ubuntu / 244 Debian
+                'label': '%s [#%s]' % (self.name, index)
             }
             
             headers = {
@@ -28,90 +45,114 @@ class VULTR:
                 'Content-Type': 'application/json'
             }
             
-            instance = self.session.post('https://api.vultr.com/v2/instances', json=data, headers=headers).json()['instance']
-            
-            password, instance_id = instance['default_password'], instance['id']
+            try:
+                instance = self.session.post(
+                    'https://api.vultr.com/v2/instances', 
+                    json=data, headers=headers
+                ).json()['instance']
+                
+                password: str = instance['default_password']
+                instance_id: str = instance['id']
+            except:
+                return False
             
             while True:
-                try:
-                    json_response = self.session.get(f'https://api.vultr.com/v2/instances/{instance_id}', headers=headers).json()['instance']
-                except:
-                    pass
+                sleep(5)
                 
-                if json_response['status'] == 'active':
-                    ip = json_response['main_ip']
-                    break
+                try:
+                    json_response = self.session.get(
+                        f'https://api.vultr.com/v2/instances/{instance_id}', 
+                        headers=headers
+                    ).json()['instance']
+                    
+                    if json_response['status'] == 'active':
+                        ip: str = json_response['main_ip']
+                        break
+                except:
+                    continue
             return ip, password, instance_id
-        except:
-            return False
     
-    def delete_instance(self, instance_id):
+    def delete_instance(self, instance_id: int) -> bool:
         headers = {
             'Authorization': 'Bearer ' + str(self.vultr_api_key)
         }
+        
         while 1:
+            sleep(10)
             try:
-                status_code = self.session.delete(f'https://api.vultr.com/v2/instances/{instance_id}', headers=headers).status_code
+                return self.session.delete(
+                    f'https://api.vultr.com/v2/instances/{instance_id}', 
+                    headers=headers
+                ).status_code == 204
             except:
-                pass
+                continue
             
-            if status_code == 204:
-                return True
-            else:
-                return False
-            
-       
-    def set_console_info(self):
-        while True:
-            print(f'[VULTR] Servers: {self.servers} | Active Servers: {len(self.active_servers)}', end="\r")
-
-    def main(self, index):
-        while True:
+    def main(self, index: int):
+        sleep(int(index) * 10) # Add some delay between the deployment of each server
+        
+        while (self.running):
             while 1:
                 data = self.create_instance(index)
-                if not data == False:
-                    ip, password, instance_id = data
-                    break
+                
+                if data is False:
+                    self.logger.warning(f"# {index}: Failed to deploy an instance, sleeping and retrying...")
+                    sleep(290)
+                    return self.delete_instance(instance_id)
+
+                ip, password, instance_id = data
+                
+                self.logger.log(f" #{index}: Successfully created an instance - {ip}!")
+                break
+
+            # Give the server some time to set up properly.                       
+            sleep(300)
             
-            # Check if the server has been deployed properly
-            if not len(instance_id) < 0:
-                print(f'[#{index}] Successfully deployed {ip}!')
-            else:
-                sleep(60)
-                return self.delete_instance(instance_id)
-            
-            sleep(90) # Give it some time to set up
-            
-            server = Manager(index, ip, password)
+            session = Assistant(index, ip, password)
             
             try:
-                # Connect to the server
+                # Connect to the actual instance.
                 while 1:
-                    if server.connect():
-                        print(f'[#{index}] Successfully connected to {ip}!')
+                    if session.connect():
+                        self.logger.log(f" #{index}: Successfully connected to {ip}!")
                         break  
                     
                 sleep(10)
                 
-                # Upload files onto the server
+                # Install dependencies and python modules.
                 while 1:
-                    if server.upload_files():
-                        print(f"[#{index}] Files have been uploaded to {ip}!")
+                    if session.install_dependencies():
+                        self.logger.log(f" #{index}: Successfully installed dependencies on {ip}!")
                         break
-    
-                # Disconnect from the server
-                while 1:
-                    if server.disconnect():
-                        print(f"[#{index}] Successfully disconnected from {ip}!")
-                        break
-                   
-                # Add your task here and let it sleep for however long you feel necessary
-                sleep(28800)
                 
-                # Download the files onto local PC
+                sleep(5)
+                
+                # Upload our local files onto the server.
                 while 1:
-                    if server.download_files():
-                        print(f"[#{index}] Successfully downloaded files from {ip}!")
+                    if session.upload_files():
+                        self.logger.log(f" #{index}: Files have been uploaded to {ip}!")
+                        break
+                
+                sleep(5)
+                
+                # Initiate our task.
+                while 1:
+                    if session.initiate_claimer(index, self.operator):
+                        self.logger.log(f" #{index}: Autoclaimer started on {ip}!")
+                        break
+                
+                # Disconnect from the server.
+                while 1:
+                    if session.disconnect():
+                        self.logger.log(f" #{index}: Successfully disconnected from {ip}!")
+                        break
+                    
+                # Sleep while our task is doing its job.
+                sleep(28800) # 8 hours
+                
+                # Connect and save our files to local.
+                while 1:
+                    if session.download_files():
+                        self.logger.log(f" #{index}: Successfully downloaded server files from {ip}!")
                         break
                 
                 sleep(5)
@@ -119,19 +160,19 @@ class VULTR:
                 # Now, after we have done what we needed, we simply get rid of the instance.
                 while 1:
                     if self.delete_instance(instance_id):
-                        print(f'[#{index}] Successfully destroyed {instance_id}!')
+                        self.logger.log(f" #{index}: Successfully destroyed our instance - {ip}!")
                         break
-                
-                # Remove the server from the active pool list
-                self.active_servers.remove(instance_id)
             except KeyboardInterrupt:
-                return self.delete_instance(instance_id)
+                self.running = False; return
     
     def init(self):
-        print('Vultr Deployer started!')
+        print("Vultr Deployer Started!\n")
         
-        for x in range(int(self.servers)):
-            Thread(target=self.main, args=(x,)).start()
+        for index in range(int(self.instances)):
+            Thread(
+                target=self.main, 
+                args=(index,)
+            ).start()
            
 if __name__ == '__main__':
-    VULTR()
+    VULTR().init()
